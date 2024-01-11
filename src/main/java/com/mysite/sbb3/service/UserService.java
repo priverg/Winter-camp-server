122c339d07c5ca7ed2bcacc436f6e3ad6d045d84
priverg163 <priverg163@dgsw.hs.kr>
2024-01-11 12:26:21 +0900

    User service 작성

---
+++ src/main/java/com/mysite/sbb3/service/UserService.java
@@ -0,0 +1,25 @@
+package com.mysite.sbb3.service;
+
+import com.mysite.sbb3.entity.SiteUser;
+import com.mysite.sbb3.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public SiteUser create(String username, String email, String password){
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setEmail(email);
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        user.setPassword(passwordEncoder.encode(password));
+        this.userRepository.save(user);
+        return user;
+
+    }
+}
