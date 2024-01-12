cb108b71804546c8c34a89be92d3820bb1c780a9
priverg163 <priverg163@dgsw.hs.kr>
2024-01-12 10:21:34 +0900

    회원가입 오류 수정

--- src/main/java/com/mysite/sbb3/controller/UserController.java
+++ src/main/java/com/mysite/sbb3/controller/UserController.java
@@ -4,6 +4,7 @@ import com.mysite.sbb3.exception.UserCreateForm;
 import com.mysite.sbb3.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,12 +26,27 @@ public class UserController {
         if(bindingResult.hasErrors()){
             return "signup_form";
         }
-        if(!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())){
+        if(!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())){ //실수를 찾기 위해 Pass1 Pass2 사용 (humanError)
             bindingResult.rejectValue("password2","passwordInCorrect", "2개의 패스워드가 일치하지 않습니다");
             return "signup_form";
         }
+        try{
+            userService.create(userCreateForm.getUsername(),userCreateForm.getEmail(),userCreateForm.getPassword1());
+        }catch(DataIntegrityViolationException e){
+            e.printStackTrace();;
+            bindingResult.reject("signupFailed","이미 등록된 사용자입니다.");
+            return "signup_form";
+        }catch(Exception e){
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
 
-        userService.create(userCreateForm.getUsername(),userCreateForm.getEmail(),userCreateForm.getPassword1());
         return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login_form";
     }
 }
